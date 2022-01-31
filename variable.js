824fac4be1e375a65ad29d42acc108b08d10dad0
sujonmia <sujonmia.0612@gmail.com>
2022-01-31 22:26:10 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,18 @@
+var age = 21;
+
+var place = 'Dhaka';
+
+// string type variable
+var name = "Sujon Al Mamun";
+
+var place = "Dhaka";
+
+// numeric type variable
+var age = 21;
+
+var price = 499;
+
+// bool type variable
+var pass = true;
+
+var ready = false;
